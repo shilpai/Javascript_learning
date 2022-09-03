005e3f2215bde7340ebaa4df1db9513de0c872c6
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-03 21:31:08 +0530

    basics

---
+++ IntroductiontoJs/index.js
@@ -0,0 +1,33 @@
+console.log("This Program add 1, 2 and 54")
+
+function addThreeNUmbers(a, b, c) {
+  return a + b + c;
+}
+let c = addThreeNUmbers(1, 2, 54)
+console.log(c)
+
+ let js = "Amazing";
+    // if (js === "Amazing") alert("Javascript is FUN!");
+
+     27 + 8 + 10 - 2
+    console.log(27 + 8 + 10 - 2);
+
+    console.log("shilpa");
+    console.log(50);
+
+    let firstName = "Sonu";
+
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+
+    let shilpa_sonu = "SS";
+    let $function = 21;
+
+    let person = "shilpa";
+    let PI = 3.1415;
+
+    let myFirstJob = "Programmer";
+    let myCurrentJob = "Photographer";
+
+    console.log(myFirstJob)
