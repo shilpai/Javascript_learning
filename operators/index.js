9ffe869e5e4c6f7214f1466c4fb30c2dc002dd91
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-04 15:13:23 +0530

    javascript operators

---
+++ operators/index.js
@@ -0,0 +1,85 @@
+// Operators
+// console.log("Operators in JS");
+//Arithmatic Operators
+// let a = 10;
+// let b = 4;
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a / b = ", a / b);
+// console.log("a * b = ", a * b);
+// console.log("a % b = ", a * b);
+// console.log("a ** b = ", a * b);
+// console.log("a++ = ", a++);
+// console.log("++a = ", ++a);
+// console.log("a = ", a);
+
+// console.log("++a = ", ++a);
+// console.log("a++ = ", a++);
+// console.log("--a = ", --a);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+// console.log("a-- = ", a--);
+
+//Assignment Operators
+// let assignment = 2;
+// assignment += 5 // same as assignment  = assignment + 5
+// console.log(assignment)
+
+//Comparision Operators
+// let comp1 = 6;
+// let comp2 = 7;
+// console.log("comp1 == comp2 is ", comp1 == comp2)
+
+// let comp1 = 6;
+// let comp2 = "6";
+// console.log("comp1 == comp2 is ", comp1 == comp2)
+// console.log("comp1 != comp2 is ", comp1 != comp2)
+// console.log("comp1 === comp2 is ", comp1 === comp2)
+// console.log("comp1 !== comp2 is ", comp1 !== comp2)
+// console.log("comp1 > comp2 is ", comp1 > comp2)
+
+//Logical Operators
+// let x = 5;
+// let y = 6;
+// console.log(x < y && x == 5) //logical and
+// console.log(x > y || x == 5) //logical or
+// console.log(!false) //logical not
+// console.log(!true) //logical not
+
+// Basic Operators
+// Math Operators
+const now = 2037;
+const ageShilpa = now - 1993;
+const ageKittu = now - 2002;
+
+console.log( ageShilpa,ageKittu)
+console.log(ageShilpa*2, ageShilpa / 10, 2**3)
+//2**3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'shilpa';
+const lastName = 'sonule';
+console.log(firstName + ' ' + lastName)
+
+// Assignment Operators
+// let x = 10 + 5; //15
+// x += 10;       // x = x + 15 = 25
+// x *= 4;       // x = x * 4 = 100
+// x ++ ;       // x = x + 1
+// x -- ;
+// x -- ;
+// console.log(x);
+
+// Comparision Operators
+console.log(ageShilpa > ageKittu); // >, <, >=, >=
+console.log(ageKittu >= 35);
+const isFullAge = ageKittu >= 35;
+
+console.log(now - 1993 > now - 2002);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x,y);
+
+const averageAge = (ageShilpa, ageKittu) / 2;
+console.log(ageShilpa,ageKittu,averageAge);
+
