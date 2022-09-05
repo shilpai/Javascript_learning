efeb984acd94d45fc5cb1d27cdc37b7ac0f74d33
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-05 16:28:28 +0530

    practice set 2

---
+++ Practice-set-2/index.js
@@ -0,0 +1,48 @@
+// Practice set 2
+// Use logical operator to find whether the age of a person lies between 10 and 20 ?
+/*let age = prompt("What is your age?")
+age = Number.parseInt()
+ if (age>10 && age<20){
+   console.log("Your age lies between 10 and 20")
+ }
+else{
+  console.log("Your age doesnt lies between 10 and 20")
+    
+}
+*/
+
+// Demostrate the use of switch case statements in Javascript 
+/*let age = prompt ("What is your age?")
+switch (age) {
+  case '12' :
+    console.log ("Your age is 12")
+    break
+  case '13' :
+    console.log ("Your age is 13")
+  case '14' :
+    console.log ("Your age is 14")
+  case '15' :
+    console.log ("Your age is 15")
+  default:
+    console.log ("Your age is not special")
+    
+}
+*/
+
+// Write a Javascript  programe to find whether a number is divisible by either 2 or 3
+/*let num = prompt("What is your age?")
+num = Number.parseInt(num)
+if(num % 2 == 0 && num % 3 == 0){
+  console.log("Your number is devisible by 2 and 3")
+}
+else{
+  console.log("Your number is not devisible by 2 and 3")
+}
+*/
+
+// write a javascript programe to find whether a number is divisible by either 2 or 3
+
+// Print "you can drive" or "you can not drive" based on age being greater than 18 using ternary operator
+let age = 19
+let a = age > 18 ? "You can drive" : "You can not drive"
+console.log(a)
