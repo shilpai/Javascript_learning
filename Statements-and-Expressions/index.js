e2e82482b0e6be56cc9c0d1442d491be6ac84355
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-13 15:30:59 +0530

    Add files via upload

---
+++ Statements-and-Expressions/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+if (23 > 100) {
+  console.log('23 number is bigger');
+} else {
+  console.log('23 number is not bigger');
+}
+
+
+
+
+
+// note -in template literals we can only insert expressions but not statements
