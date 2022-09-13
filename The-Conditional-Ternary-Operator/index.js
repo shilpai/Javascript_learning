108a4f969773c5d1fa8f978d9bf2d385e84bc8bc
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-13 15:30:20 +0530

    Add files via upload

---
+++ The-Conditional-Ternary-Operator/index.js
@@ -0,0 +1,17 @@
+// Ternary Operator
+const age = 20;
+// age >= 18? console.log('I like to drink wine🍷') : console.log('I like to drink water💧'); 
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💦 ';
+console.log(drink);
+
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷'
+} else {
+  drink2 = 'water 💦 '
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💦'}`);
