74f2280ba35f3e5de7429721ead61c6aec42585a
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-14 14:51:17 +0530

    Add files via upload

---
+++ Activating-Strict-Mode/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive 😎');
+
+// const interface = 'Audio';
+// const private = 534;
