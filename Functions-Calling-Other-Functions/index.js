c2bbd6de216b0429934e407e3937b3d6aefad3bd
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-17 22:00:39 +0530

    first commit 🚀

---
+++ Functions-Calling-Other-Functions/index.js
@@ -0,0 +1,14 @@
+//
+const cutPieces = function(fruit) {
+  return fruit * 4;
+};
+
+const fruitProcessor = function(apples, oranges) {
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+};
+
+console.log(fruitProcessor(2, 3));
