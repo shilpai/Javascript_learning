7cb264143986a24dd335a753a3811fded9a9744c
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2022-09-21 22:13:57 +0530

    commit first  🚀

---
+++ Introduction-to-Objects/index.js
@@ -0,0 +1,21 @@
+// Objects
+
+// const shilpaArray = [
+//   'shilpa',
+//   'sonule',
+//   'Navi Mumbai',
+//   28,
+//   ['Trisha', 'Shivani', 'Priyanka']
+// ]
+
+// console.log(shilpaArray);
+
+const shilpas = {
+  firstName: 'shilpa',
+  lastName: 'sonule',
+  age: 28,
+  friends: ['Trisha', 'Shivani', 'Priyanka']
+}
+
+console.log(shilpas);
+
