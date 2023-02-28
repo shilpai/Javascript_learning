576cc266ce537d5ad335cf6c52e00f16d4d6eea1
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2023-02-28 16:02:38 +0530

    first commit

---
+++ Strings/script.js
@@ -0,0 +1,30 @@
+// String Lengths and Retrieving the Number of Characters
+/*
+var tweet = prompt("compose your tweet");
+// var tweetCount = tweet.length;
+// alert("you have written" + " " + tweetCount + " characters, you have " + (140 - tweetCount) + " " +"characters reaming");
+alert(" you have written " + tweet.length + " Characteres you have " + (140 - tweet.length) + " " + "character remaining");
+*/
+
+// Slicing and Extracting Parts of a String
+/*
+var tweet = 'Shilpa';
+console.log(tweet.slice(0,2));
+
+var tweet = prompt("compose your tweet");
+var tweetUnder140 = tweet.slice(0,140);
+alert(tweetUnder140);
+*/
+// alert(prompt("compose your tweet").slice(0,140));
+
+//  Challenge: Changing Casing in Text
+/*
+var names = "shilpa";
+var result =names.toUpperCase();
+console.log(result);
+
+var result2 = names.toLowerCase();
+console.log(result2);
+*/
+
+//  Challenge: Changing String Casing Solution
